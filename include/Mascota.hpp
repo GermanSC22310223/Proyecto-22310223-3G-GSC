be9094dc006fd43ca386661136b6d2f3a2b03846
GermanSC22310223 <143230150+GermanSC22310223@users.noreply.github.com>
2023-09-02 01:05:06 +0000

    1-Septeimbre-2023

---
+++ include/Mascota.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include<iostream>
+class Mascota
+{
+private:
+    int vida;
+    std::string nombre;
+public:
+    Mascota()
+    {
+        this->vida = 100;
+    }
+    void Establecer_Nombre(std::string nombre) 
+    {
+        this->nombre = nombre;
+    }
+    ~Mascota() {}
+    void DecirNomnbre(){
+        std::cout<<"Mi nombre es :"<<this->nombre<<std::endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
-
+#include <iostream>
+#include<Mascota.hpp>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::cout<<"Juego de Mascotas"<<std::endl;
+    
+    Mascota m1,m2,m3;
+    m1.Establecer_Nombre("Negro");
+    m2.Establecer_Nombre("Pliar");
+    m3.Establecer_Nombre("Tercero");
+    m1.DecirNomnbre();
+    m2.DecirNomnbre();
+    m3.DecirNomnbre();
     return 0;
 }
 
-for (int i = 0; i < count; i++)
-{
-    /* code */
-}
